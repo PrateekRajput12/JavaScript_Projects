31511deaf1145c279375e5b9c8f1f842c14640a4
PrateekRajput12 <ficredinn@gmail.com>
2024-07-18 11:39:41 +0530

    Counter Projects

---
+++ Counter/App.js
@@ -0,0 +1,15 @@
+// const data=document.querySelector("ans")
+
+function minus(){
+    const data=document.querySelector(".ans")
+    const newdata=parseInt(data.innerHTML);
+    data.innerHTML=newdata-1
+
+}
+
+function add(){
+    const data=document.querySelector(".ans")
+    const newdata=parseInt(data.innerHTML);
+    data.innerHTML=newdata+1
+    
+}
